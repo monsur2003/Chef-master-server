8ff6eaa07e0a5cc82d22d8102cb871af29eba91a
monsur776e <monsuralam8897@gmail.com>
2023-05-02 12:01:39 +0600

    hello world print succesfully

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
    res.send("Hello, world!");
+   console.log("server runnign");
+});
+app.listen(PORT, (req, res) => {
+   console.log(`server listening on ${PORT}`);
 });

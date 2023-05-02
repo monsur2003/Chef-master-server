6d66e7e43400c6ce7bdd2f6fe382cf9dfabddb73
monsur776e <monsuralam8897@gmail.com>
2023-05-03 02:48:46 +0600

    single id find done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
 
 const chefs = require("./data/cheifs.json");
 

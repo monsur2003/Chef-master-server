f12aa605a524abf91b682c29a6f7500d9e86e5fb
monsur776e <monsuralam8897@gmail.com>
2023-05-02 23:22:06 +0600

    chefs json create successfull

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const chefs = require("./data/cheifs.json");
+
 app.get("/", (req, res) => {
    res.send("Hello, world!");
    console.log("server runnign");
 });
+
+app.get("/chefs", (req, res) => {
+   res.send(chefs);
+});
+app.get("/chefs/:id", (req, res) => {
+   const id = req.params.id;
+   const singleChef = chefs.find((chef) => chef.id == id);
+   res.send(singleChef);
+});
+
 app.listen(PORT, (req, res) => {
    console.log(`server listening on ${PORT}`);
 });
